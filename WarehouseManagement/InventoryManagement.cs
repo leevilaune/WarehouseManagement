cda57a59e0814ce5635433576cc901b5cfdca16c
nuuskapoeka <npoeka@gmail.com>
2022-08-26 10:23:31 +0300

    added classes for database connection and usage

---
+++ WarehouseManagement/InventoryManagement.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarehouseManagement
+{
+    internal class InventoryManagement : DbContext
+    {
+        public DbSet<Product>? Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            Console.Write("DB Password: ");
+            string password = Console.ReadLine();
+
+            string connection = "Data Source=.;" +
+                                "Initial Catalog=InventoryManagement;" +
+                                "Id:sa;" + $"Password{password};" +
+                                "MultipleActiveResultSets=true;";
+
+            optionsBuilder.UseSqlServer(connection);
+        }
+    }
+}
